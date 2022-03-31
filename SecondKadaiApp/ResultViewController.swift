3dcba7a8108f972836f51262b10d891d4bf18034
tetA <doublepotato-temporarygittempo@yahoo.co.jp>
2022-03-31 17:44:02 +0900

    とりあえずAutoレイアウト以外の課題の条件はクリアできた。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 矢野俊作 on 2022/03/25.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var name:String = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let result = name
+        label.text = "こんにちは、\(result)さん"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
